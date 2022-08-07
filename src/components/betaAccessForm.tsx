6d1c370586e88b0a8941a0c3e9c11332e385c073
Robbert <robberttg@protonmail.com>
2022-08-07 11:05:22 +0200

    Updates

--- src/components/betaAccessForm.tsx
+++ src/components/betaAccessForm.tsx
@@ -1,43 +1,62 @@
 import React, { useState } from 'react'
 import { useFormspark } from '@formspark/use-formspark'
+import { FiCheck } from 'react-icons/fi'
 
-const FORMSPARK_FORM_ID = process.env.FORMSPARK_FORM_ID
+const FORMSPARK_FORM_ID = process.env.NEXT_PUBLIC_FORMSPARK_FORM_ID
 
 const BetaAccesForm = ({ scrollY }: { scrollY: number }) => {
   const [submit, submitting] = useFormspark({
     formId: FORMSPARK_FORM_ID ?? '',
   })
-
+  const [complete, setComplete] = useState(false)
   const [message, setMessage] = useState('')
+  const [error, setError] = useState(false)
 
   const onSubmit = async (e: any) => {
     e.preventDefault()
-    const response = await submit({ message })
-    console.log(response)
+    try {
+      console.log(message)
+      console.log(FORMSPARK_FORM_ID)
+      const response = await submit({ message })
+      console.log(response)
+      setComplete(true)
+    }
+    catch (err) {
+      setError(true)
+    }
     // alert('Form submitted')
   }
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className={`flex flex-row w-screen max-w-2xl items-center shadow-sm p-2 my-6 bg-gray-200 rounded-lg b-2 mt-14 justify-between ${
-        scrollY > 0 && 'shadow-lg sticky top-16'
-      } transition-all duration-1000`}
-    >
-      <input
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        className="h-14 mr-4 bg-gray-200 pl-2 outline-0 grow"
-        placeholder="enter your email"
-      />
-      <button
-        type="submit"
-        disabled={submitting}
-        className="p-4 bg-orange-400 rounded-lg font-semibold text-white shadow-sm"
+    <>
+      <form
+        onSubmit={onSubmit}
+        className={`flex flex-row w-screen max-w-2xl items-center shadow-sm p-2 my-6 bg-gray-200 rounded-lg b-2 mt-14 ${ !complete ? 'justify-between' : 'justify-center' } ${ scrollY > 0 && 'shadow-lg sticky top-16 z-50'
+          } transition-all duration-1000`}
       >
-        Request access
-      </button>
-    </form>
+        {!complete && (
+          <>
+            <input
+              type="email"
+              required
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="h-14 mr-4 bg-gray-200 pl-2 outline-0 grow"
+              placeholder="enter your email"
+            />
+            <button
+              type="submit"
+              disabled={submitting}
+              className="p-4 bg-orange-400 hover:bg-orange-500 rounded-lg font-semibold text-white shadow-sm transition-all"
+            >
+              Request access
+            </button>
+          </>
+        )}
+        {complete && <div className="flex flex-row items-center h-14"><FiCheck size={20} /><span className="ml-4">You will be contacted shortly</span></div>}
+      </form>
+      {error && <span className="text-orange-600 mb-4">Something went wrong, reach out to us on <a href="https://join.slack.com/t/slack-pfs5354/shared_invite/zt-1dnnwr9wn-njkaaxES_sUWywV2~JANjg" className="underline">Slack</a></span>}
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,9 +25,8 @@ const Home: NextPage = () => {
       <div className="flex flex-row p-12">
         <div className="max-w-7xl flex flex-row justify-center flex-wrap items-center w-full mx-auto h-96 select-none">
           <h1
-            className={`text-8xl ${
-              scrollY > 0 && 'text-gray-200'
-            } transition-all duration-1000`}
+            className={`text-8xl ${ scrollY > 0 && 'text-gray-200'
+              } transition-all duration-1000`}
           >
             {COMPANY_TITLE1}
             <span className="font-semibold">{COMPANY_TITLE2}</span>
@@ -57,9 +56,8 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div
-        className={`max-w-7xl mx-auto w-full ${
-          scrollY > 0 ? 'opacity-100' : 'opacity-30'
-        } transition-all duration-1000`}
+        className={`max-w-7xl mx-auto w-full ${ scrollY > 0 ? 'opacity-100' : 'opacity-30'
+          } transition-all duration-1000`}
       >
         <h2 className="mt-20">{PROJECT_HEADER}</h2>
         <div className=" border-2 p-6 rounded-lg my-6">
@@ -92,7 +90,7 @@ const Home: NextPage = () => {
               <Image
                 src={JunoLogo}
                 alt="Juno Email Manager"
-                className="cursor-pointer"
+                className="cursor-pointer invert"
               />
             </Link>
           </div>
