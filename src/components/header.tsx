d268cb1f34a6e04cb9c3bc6495ef5febc30e2a23
Robbert <robberttg@protonmail.com>
2022-08-10 11:21:48 +0200

    Add dprint + small fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 z-10 w-full p-4 border-b ${
+      className={`fixed top-0 left-0 z-20 w-full p-4 border-b ${
         scrollY > 0 ? 'border-gray-300' : 'border-transparent'
       } bg-gray-100/80 transition-all duration-200 backdrop-blur-lg`}
     >
