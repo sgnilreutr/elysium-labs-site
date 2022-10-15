6628797cf9767c877aaf19a249c54b28d80e730e
Robbert <robberttg@protonmail.com>
2022-10-15 20:00:38 +0200

    Update googleButton.tsx

--- src/components/googleButton/googleButton.tsx
+++ src/components/googleButton/googleButton.tsx
@@ -7,7 +7,7 @@ const GoogleButton = () => {
     <a
       href={process.env.NEXT_PUBLIC_JUNO_GOOGLE_LOGIN}
       target="_blank"
-      className="flex flex-row items-center p-3 font-semibold text-white transition-all bg-black rounded-md hover:bg-neutral-800 bg:shadow-lg"
+      className="flex flex-row items-center px-3 font-semibold text-white transition-all bg-black rounded-md h-14 hover:bg-neutral-800 bg:shadow-lg"
       rel="noreferrer"
     >
       <div className="p-1">
