5b8dbad640dd5d8515a9e4bf3bcd0e79ed286876
Robbert <robberttg@protonmail.com>
2022-12-12 22:33:04 +0100

    Add docs links

--- src/constants/menuConstants.tsx
+++ src/constants/menuConstants.tsx
@@ -1,7 +1,8 @@
-import { FaGithub, FaDiscord } from 'react-icons/fa'
+import { FaGithub, FaDiscord, FaBook } from 'react-icons/fa'
 
 const ICON_SIZE = 20
 const DISCORD_LINK = process.env.NEXT_PUBLIC_DISCORD_INVITE_LINK
+const DOCS_LINK = process.env.NEXT_PUBLIC_DOCS_LINK
 
 export const MENU_ITEMS = [
   { name: 'Juno', link: '/juno' },
@@ -15,6 +16,11 @@ export const EXTERNAL_ITEMS = [
     name: <FaGithub size={ICON_SIZE} />,
     link: 'https://github.com/Elysium-Labs-EU',
     title: 'Github',
+  },
+  {
+    name: <FaBook size={ICON_SIZE} />,
+    link: DOCS_LINK,
+    title: 'Docs',
   },
   {
     name: <FaDiscord size={ICON_SIZE} />,
